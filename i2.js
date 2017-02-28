5197271c97e148c9f02e0184fefaa8344e5fbce7
Dimitry Ivanov <d.ivanov@argest.com.ua>
2017-02-28 12:56:23 +0200

    betta

--- i2.js
+++ i2.js
@@ -1,4 +1,5 @@
 'use strict';
+var fs = require('fs');
 
 var Raml = require('./index');
 var raml = new Raml({
@@ -28,13 +29,23 @@ raml.methods('books', 'post', {
   description: 'Add new book',
   body: {
       'application/json': {
-          schema: 'books',
+          type: 'books',
           example: { name: 'one', author: { name: 'Art' } },
         },
     },
   responses: {
     201: { 'application/json': { name: 'one', author: { name: 'Art' } } },
     400: { 'application/json': { code: '221', message: 'name not matched with string type' } },
+  },
+});
+
+raml.methods('books/:bookId/readers', {
+  get: {
+    description: 'Get information about who has read {bookId} book',
+    responses: {
+      200: { 'application/json': [{ _id: 343, name: 'Mike' },{ _id: 324, name: 'John'}] },
+      404: { 'application/json': { code: '121', message: 'Readers not found' } },
+    },
   },
 });
 
@@ -56,8 +67,23 @@ raml.methods('books/:bookId', {
   },
 });
 
-var ramlText = raml.generate();
-console.log(ramlText);
+raml.generate(function (err, ramlText) {
+
+  console.log(ramlText);
+
+  fs.writeFile('test-raml2html.raml', ramlText, function (err) {
+    console.log('===== raml2html parsing =====');
+    var raml2html = require('raml2html');
+    var configWithDefaultTheme = raml2html.getConfigForTheme();
+    raml2html.render('test-raml2html.raml', configWithDefaultTheme).then(function (result) {
+      console.log('test-raml2html passed');
+    }, function (error) {
+
+      console.log('Error raml2html parsing: ' + error);
+    });
+  });
+
+});
 
 //var raml2 = new Raml();
 //console.log(raml2.generate());
--- index.js
+++ index.js
@@ -35,15 +35,46 @@ exports = module.exports = function (options) {
 
     generate: function (cb) {
       var _this = this;
-      fs.readFile('template/raml_1_0.mustache', function (err, template) {
+
+      var url = Object.keys(methodsData).sort();
+      var methodsTree = {};
+      for( var i = url.length-1; i>=0; i-- ) {
+        for( var j = i-1; j>=0; j-- ) {
+          if(typeof methodsTree[url[i]] === 'undefined' && url[i].indexOf(url[j]+'/') == 0){
+console.log(`${url[i]} -> ${url[j]}`);
+            methodsTree[url[i]] = {
+              shortLink : url[i].replace(url[j]+'/',''),
+              deep: url[j].split('/').length
+            };
+          }
+        }
+        if(typeof methodsTree[url[i]] === 'undefined'){
+          methodsTree[url[i]] = {shortLink: url[i], deep: 0};
+        }
+      }
+console.log(methodsTree);
+console.log(methodsData);
+      fs.readFile('template/raml_1_0.nunjucks', function (err, template) {
         if (err) {
           cb(err);
         } else {
           var view = {
             options: options,
             typesData: typesData,
+            methods: url,
+            methodsTree: methodsTree,
+            methodsData: methodsData,
             stringify: function (value, replacer, space, indent) {
               return JSON.stringify(value, replacer, space).replace(/^/gm, Array(indent).join(' '));
+            },
+            updatePath: function (path) {
+              return path.replace(/\/:([^/:-]+)/g, '/{$1}');
+            },
+            regReplace: function( what, from, to, flags ) {
+              return what.replace( new RegExp(from, flags), to );
+            },
+            repeat: function( what, count ) {
+              return Array(count+1).join(what)
             },
           };
           cb(null, nunjucks.renderString(template.toString(), view));
